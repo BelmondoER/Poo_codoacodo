e4d230a9734cccf34e956a4cfbe9342406399198
Eduardo <belmondoer@gmail.com>
2020-12-13 16:07:46 -0300

    Iniciando repo POO

---
+++ main.py
@@ -0,0 +1,97 @@
+from problema1 import Persona
+from problema10 import Punto2
+from problema11 import Familia
+from problema2 import Alumno
+from problema3 import Empleado
+from problema4 import Punto
+from problema5 import Operaciones
+from problema6 import AlumnoyNota
+from problema7 import Cliente, Banco
+from problema8 import Dado, JuegoDeDados
+from problema9 import Cliente2
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Ejercicio 1
+    """
+    print_hi('PyCharm')
+    persona = Persona()
+    persona.inicializar_nombre("Juan")
+    persona.imprimir_nombre()
+    print(persona.nombre)
+
+    # Ejercicio 2
+
+    alumno1 = Alumno()
+    alumno1.inicializar("Pedro", 2)
+    alumno1.imprimir_dato()
+    alumno1.mostrar_estado()
+
+    alumno2 = Alumno()
+    alumno2.inicializar("ana", 8)
+    alumno2.imprimir_dato()
+    alumno2.mostrar_estado()
+
+    # Ejercicio 3
+
+    empleado1 = Empleado()
+    empleado1.imprimir_datos()
+    empleado1.pagar_impuestos()
+   
+    # Ejercicio 4
+
+    coordenada = Punto(0, 0)
+    coordenada.imprimir_cuadrante()
+    
+    # Ejercicio 5
+
+    operacion = Operaciones()
+    
+    # Ejercicio 6 
+    AlumnoyNota().elegir_opcion()
+    
+    # Ejercicio 7
+    
+    galaxia = Banco("Galaxia")
+    galaxia.operar()
+    print(galaxia)
+    
+    # Ejercicio 8
+    
+    juego = JuegoDeDados()
+    
+    # Ejercicio 9
+
+    cliente = Cliente2(24, "Juan")
+    cliente2 = Cliente2(34, "jhonn")
+    cliente3 = Cliente2(244, "Jucen")
+    cliente4= Cliente2(245, "rob")
+    cliente5 = Cliente2(246, "luke")
+    print(cliente)
+    print(cliente2)
+    print(cliente3)
+    print(cliente4)
+    print(cliente5)
+    
+
+    # Ejercicio 10
+
+    punto1 = Punto2("Hola", "Carlitox")
+    punto2 = Punto2("hola", "vengo a venir")
+    print(punto1)
+    print(punto2)
+   
+    # Ejercicio 11
+
+    familia1 = Familia("Jose", "Maria", ["Juana", "juan"])
+
+    print(familia1)
+     """
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
